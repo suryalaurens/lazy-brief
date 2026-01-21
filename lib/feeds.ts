8cefe7b23df71add64d72d8b4f6ad57ac768238c
suryalaurens <suryalaurens@gmail.com>
2026-01-21 16:09:13 +0700

    Create feeds.ts

---
+++ lib/feeds.ts
@@ -0,0 +1,9 @@
+export const GLOBAL_FEEDS = [
+  "https://www.reuters.com/world/rss",
+  "https://apnews.com/rss"
+];
+
+export const INDONESIA_FEEDS = [
+  "https://www.antaranews.com/rss/terkini",
+  "https://www.reuters.com/world/asia-pacific/indonesia/rss"
+];
